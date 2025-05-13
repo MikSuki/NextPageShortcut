d5f41d28b1164a962ecf957c66850dfc5dce852a
MikSuki <34766025+MikSuki@users.noreply.github.com>
2025-05-14 01:29:46 +0800

    feat: show toast when going to next page

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,5 @@
+import Toastify from "toastify-js"
+
 let lastClickedElement: HTMLElement | null
 
 document.addEventListener("contextmenu", e => {
@@ -125,5 +127,27 @@ function goNextPage() {
         console.log(`${xpath} `);
         console.log(target);
         target.click()
+
+        Toastify({
+            text: "next page",
+            duration: 600,
+            gravity: "top",
+            position: "left",
+            style: {
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                background: "#333",
+                color: "#fff",
+                padding: "1em 2em",
+                borderRadius: "8px",
+                boxShadow: "0 4px 10px rgba(0, 0, 0, 0.3)",
+                zIndex: "9999",
+                textAlign: "center",
+                whiteSpace: "nowrap"
+            },
+            stopOnFocus: false
+        }).showToast()
     })
 }
