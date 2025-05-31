e70806b233592d4d77c427915472f0fb246b51e0
MikSuki <34766025+MikSuki@users.noreply.github.com>
2025-06-01 02:16:52 +0800

    refactor: disable log for production

--- src/Log.ts
+++ src/Log.ts
@@ -1,7 +1,9 @@
-function show(...args: any[]) {
-    args.forEach(arg => console.log(arg))
-}
+const isDebugMode = false
 
+function show(...args: any[]) {
+    if(isDebugMode)
+        args.forEach(arg => console.log(arg))
+}
 
 export const Log = {
     show
